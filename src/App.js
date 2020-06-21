3408b4c2308ece41ad1dbf3bc2ef43fee00e5709
filipesaretta <filipessm124@gmail.com>
2020-06-21 15:11:59 +0200

    Add list repositories

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
   }
